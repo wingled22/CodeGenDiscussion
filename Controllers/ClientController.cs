f9ca8b0d2206cef047d067b1cd63e505cecf4b4a
wingled22 <pelayowindel@gmail.com>
2024-04-12 17:28:35 +0800

    add viewmodel for data anotate required

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CodeGen.Entities;
+using CodeGen.ViewModel;
 
 namespace CodeGen.Controllers
 {
@@ -21,9 +22,40 @@ namespace CodeGen.Controllers
         // GET: Client
         public async Task<IActionResult> Index()
         {
-              return _context.ClientInfos != null ? 
-                          View(await _context.ClientInfos.ToListAsync()) :
-                          Problem("Entity set 'PelayoCoopContext.ClientInfos'  is null.");
+            var client = (
+                from clientInfo in _context.ClientInfos
+                join usertype in _context.UserTypes
+                on clientInfo.UserType equals usertype.Id
+
+                select new ClientInfoViewModel
+                {
+                    Id = clientInfo.Id,
+                    UserType = clientInfo.UserType,
+                    UserTypeName = usertype.Name,
+                    FirstName = clientInfo.FirstName,
+                    MiddleName = clientInfo.MiddleName,
+                    LastName = clientInfo.LastName,
+                    Address = clientInfo.Address,
+                    ZipCode = clientInfo.ZipCode,
+                    Birthdate = clientInfo.Birthdate,
+                    Age = clientInfo.Age,
+                    NameOfFather = clientInfo.NameOfFather,
+                    NameOfmother = clientInfo.NameOfmother,
+                    CivilStatus = clientInfo.CivilStatus,
+                    Religion = clientInfo.Religion,
+                    Occupation = clientInfo.Occupation,
+                }
+            ).AsAsyncEnumerable();
+
+
+            if (client != null)
+                return View(client);
+            else
+                return Problem("walay clients");
+
+            //   return _context.ClientInfos != null ? 
+            //               View(await _context.ClientInfos.ToListAsync()) :
+            //               Problem("Entity set 'PelayoCoopContext.ClientInfos'  is null.");
         }
 
         // GET: Client/Details/5
@@ -50,12 +82,9 @@ namespace CodeGen.Controllers
             return View();
         }
 
-        // POST: Client/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserType,FirstName,MiddleName,LastName,Address,ZipCode,Birthdate,Age,NameOfFather,NameOfmother,CivilStatus,Religion,Occupation")] ClientInfo clientInfo)
+        public async Task<IActionResult> Create(ClientInfo clientInfo)
         {
             if (ModelState.IsValid)
             {
@@ -66,7 +95,6 @@ namespace CodeGen.Controllers
             return View(clientInfo);
         }
 
-        // GET: Client/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.ClientInfos == null)
@@ -74,7 +102,25 @@ namespace CodeGen.Controllers
                 return NotFound();
             }
 
-            var clientInfo = await _context.ClientInfos.FindAsync(id);
+            var clientInfo = await _context.ClientInfos.Where(x => x.Id == id)
+                                            .Select(x => new ClientInfoViewModel
+                                            {
+                                                Id = x.Id,
+                                                UserType = x.UserType,
+                                                FirstName = x.FirstName,
+                                                MiddleName = x.MiddleName,
+                                                LastName = x.LastName,
+                                                Address = x.Address,
+                                                ZipCode = x.ZipCode,
+                                                Birthdate = x.Birthdate,
+                                                Age = x.Age,
+                                                NameOfFather = x.NameOfFather,
+                                                NameOfmother = x.NameOfmother,
+                                                CivilStatus = x.CivilStatus,
+                                                Religion = x.Religion,
+                                                Occupation = x.Occupation,
+                                            })
+                                            .FirstOrDefaultAsync();
             if (clientInfo == null)
             {
                 return NotFound();
@@ -149,14 +195,14 @@ namespace CodeGen.Controllers
             {
                 _context.ClientInfos.Remove(clientInfo);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool ClientInfoExists(int id)
         {
-          return (_context.ClientInfos?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.ClientInfos?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
---
+++ ViewModel/ClientInfoViewModel.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CodeGen.ViewModel
+{
+    public class ClientInfoViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int? UserType { get; set; }
+
+        public string UserTypeName { get; set; } = "";
+
+        [Required]
+        public string? FirstName { get; set; }
+        [Required]
+        public string? MiddleName { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required]
+        public int? ZipCode { get; set; }
+        [Required]
+        public DateTime? Birthdate { get; set; }
+        [Required]
+        public int? Age { get; set; }
+        [Required]
+        public string? NameOfFather { get; set; }
+        [Required]
+        public string? NameOfmother { get; set; }
+        [Required]
+        public string? CivilStatus { get; set; }
+        [Required]
+        public string? Religion { get; set; }
+        [Required]
+        public string? Occupation { get; set; }
+    }
+}
